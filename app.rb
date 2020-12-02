14260fe83635d9213bbd62fa624fd37354463fb2
your_name <your_email@example.com>
2020-12-02 13:43:30 -0500

    commit tree 7ba01f670bbfe17b7cbaa9709c0a389e8925de98

--- app.rb
+++ app.rb
@@ -2,5 +2,48 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+    "#{@name}"
+  end
 
+  get '/square/:number' do 
+    @square_number = params[:number].to_i** 2 
+    @square_number.to_s 
+
+  end 
+
+  get '/say/:number/:phrase' do
+  @num = params[:number].to_i
+  @phrase = params[:phrase]
+  string = ""
+
+  @num.times { string += "#{@phrase}\n" }
+  string
+
+  end 
+   
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end 
+
+  get '/:operation/:number1/:number2' do 
+    @number_1 = params[:number1].to_i
+    @number_2 = params[:number2].to_i
+    if params[:operation] == "add"
+      "#{@number_1+@number_2}"
+    elsif params[:operation] == "subtract"
+      "#{@number_1-@number_2}"
+    elsif params[:operation] == "multiply"
+      "#{@number_1*@number_2}"
+    elsif params[:operation] == "divide"
+      "#{@number_1/@number_2}"
+    end
+  end
 end
